78a1a38af6eb5b679ef706c12df8e4ef5a091548
desarrolloUGIT <desarrollo.ugit@gmail.com>
2022-06-13 15:45:17 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'emergenciaApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
